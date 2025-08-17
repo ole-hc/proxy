30d0975e40666e73c222958d453cc42a666616ea
CodmOH <hofmann.ole.eibach@gmail.com>
2025-08-17 18:33:02 +0200

    Create basic TCP server with multithreading

---
+++ bibs.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <thread>
+
+#include <csignal>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+using namespace std;
